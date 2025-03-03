4aeeea1293362c02a1d497d1fb88deb0aca986fc
Margarita <margaritagarrido95@hotmail.com>
2025-03-03 12:07:56 +0100

    aproximación

---
+++ src/main.ts
@@ -0,0 +1,29 @@
+function sumar () : void{
+    const sumar = document.getElementById("sumar");
+    const resultado = parseInt("sumar") , ++1;
+    buttonSumar.addEventListener("click", sumar);
+}
+
+function restar () : void{
+    const restar = document.getElementById("restar");
+    const resultado = parseInt("restar") , --1;
+    buttonRestar.addEventListener("click", restar);
+}
+
+
+function reset (): void{
+    const reset = document.getElementById("reset");
+    const resultado = parseInt("reset") ==0;
+    buttonReset.addEventListener("click", reset);
+}
+
+function manual (): void{
+    const marcador = document.getElementById("marcador");
+    const resultado = parseInt("manual");
+    IdMarcador.addEventListener("click", );
+
+}
+
+
+padStart
+
