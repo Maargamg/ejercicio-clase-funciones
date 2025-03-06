3b2b179c2aed8dc932dc2fcf36213d1d2f95c844
Margarita <margaritagarrido95@hotmail.com>
2025-03-06 15:58:31 +0100

    no-funciona

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,23 @@
-function sumar () : void{
-    const sumar = document.getElementById("sumar");
+function turnoMarcador () : void{
+    const sumar = document.getElementById("sumar"), ++1;
     const resultado = parseInt("sumar") , ++1;
     buttonSumar.addEventListener("click", sumar);
-}
 
-function restar () : void{
+
     const restar = document.getElementById("restar");
     const resultado = parseInt("restar") , --1;
     buttonRestar.addEventListener("click", restar);
-}
 
 
-function reset (): void{
     const reset = document.getElementById("reset");
     const resultado = parseInt("reset") ==0;
     buttonReset.addEventListener("click", reset);
-}
 
-function manual (): void{
-    const marcador = document.getElementById("marcador");
+
+    const manual = document.getElementById("marcador");
     const resultado = parseInt("manual");
     IdMarcador.addEventListener("click", );
+};
 
-}
-
-
-padStart
+ /*padStart*/
 
