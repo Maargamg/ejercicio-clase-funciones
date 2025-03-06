8d0663ca3359fd8abdb445fe0a16bfcdcb693332
Margarita <margaritagarrido95@hotmail.com>
2025-03-06 16:53:03 +0100

    no-funciona

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,29 @@
 function turnoMarcador () : void{
-    const sumar = document.getElementById("sumar"), ++1;
+    const sumar = document.getElementById("sumar");
     const resultado = parseInt("sumar") , ++1;
     buttonSumar.addEventListener("click", sumar);
 
 
     const restar = document.getElementById("restar");
-    const resultado = parseInt("restar") , --1;
+    const resultado: number = parseInt("restar") , --1;
     buttonRestar.addEventListener("click", restar);
 
 
     const reset = document.getElementById("reset");
-    const resultado = parseInt("reset") ==0;
+    const resultado: number = parseInt("reset") ==0;
     buttonReset.addEventListener("click", reset);
 
 
-    const manual = document.getElementById("marcador");
-    const resultado = parseInt("manual");
+    const manual: HTMLElement | null = document.getElementById("marcador");
+    const resultado: number = parseInt("manual");
     IdMarcador.addEventListener("click", );
+
+    document.getElementById("marcador").innerHTML = resultado
+
+    if (resultado !== null && resultado !== undefined){
+        document.getElementById("marcador").innerHTML = resultado;
+        marcadorElement.innerHTML = resultado;
+    }
 };
 
  /*padStart*/
