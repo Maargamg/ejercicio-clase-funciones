37b1e623570325dd94ebab04a35a8add191f7d4e
Margarita <margaritagarrido95@hotmail.com>
2025-03-07 12:54:37 +0100

    sumayresta

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ let turno = 0;
 const botonSiguiente = document.getElementById('sumar');
 const sumar = () => {
   turno++;
-  
-  const elementoParrafo = document.getElementById('turno');
+const elementoParrafo = document.getElementById('turno');
   
   if (elementoParrafo !== null && elementoParrafo !== undefined && elementoParrafo instanceof HTMLParagraphElement) {
     elementoParrafo.textContent = turno.toString().padStart(2, "0");
@@ -13,6 +12,25 @@ if (botonSiguiente !== null && botonSiguiente !== undefined && botonSiguiente in
   botonSiguiente.addEventListener('click', () => {
     sumar();
   })
+};
+
+const botonAnterior = document.getElementById('restar');
+const restar = () => {
+    turno--;
+const elementoParrafo = document.getElementById('turno');
+
+  if (elementoParrafo !== null && elementoParrafo !== undefined && elementoParrafo instanceof HTMLParagraphElement) {
+    elementoParrafo.textContent = turno.toString().padStart(2, "0");
+  }
 }
+if (botonAnterior !== null && botonAnterior !== undefined && botonAnterior instanceof HTMLButtonElement) {
+    botonAnterior.addEventListener('click', () => {
+    restar();
+})
+};
+
+
+
+
 
 
